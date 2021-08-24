a68683392988b36db296c6fb16734e918d05ca42
tmayank860 <tmayank860@gmail.com>
2021-08-24 10:21:25 +0530

    Random COlor Generator

---
+++ ColorGenerator/index.js
@@ -0,0 +1,15 @@
+var colorpad=document.getElementById("colorpad");
+var color=document.getElementById("color");
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
+  function setRandomColor() {
+   colorpad.style.backgroundColor= getRandomColor();
+   color.innerText=(getRandomColor());
+  }
